1310e347ea6af7c0aa723559308c862d66e53961
Ivan Kavalerov <ivan@kavalerov.net>
2022-07-22 10:13:45 +0100

    Added examples of channel capabilities

--- examples/showcase-all-resources.tf
+++ examples/showcase-all-resources.tf
@@ -16,8 +16,8 @@ resource "ably_api_key" "api_key_1" {
   app_id = ably_app.app1.app_id
   name   = "KeyName"
   capability = {
-    channel1 = [""]
-    channel2 = [""]
+    channel1 = ["publish", "subscribe"]
+    channel2 = ["history"]
   }
 }
 
