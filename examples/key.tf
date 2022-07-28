dfcdb4af4a086a70e41bb975321ca68afe163dd0
Graham Russell <graham.russell@ably.com>
2022-07-28 14:39:02 +0100

    Create new resource ably_api_key_capability Rather than nesting capabilities within the ably_api_key resource, opt for a distinct resource instead to improve readability. Nesting capabilities would likely get messy if many capabilities were added.

--- examples/key.tf
+++ examples/key.tf
@@ -1,8 +1,16 @@
 resource "ably_api_key" "api_key_1" {
   app_id = ably_app.app1.app_id
   name   = "KeyName"
-  capability = {
-    channel1 = ["publish", "subscribe"]
-    channel2 = ["history"]
-  }
+}
+
+resource "ably_api_key_capability" "channel1" {
+  api_key = ably_api_key.api_key_1.name
+  resource = "channel1"
+  capabilities = ["publish", "subscribe"]
+}
+
+resource "ably_api_key_capability" "channel2" {
+  api_key = ably_api_key.api_key_1.name
+  resource = "channel2"
+  capabilities = ["history"]
 }
