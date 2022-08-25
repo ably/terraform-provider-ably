5a709ca1f9ecfba191efd32242fee24319712ccb
Morgan Adamiec <morgan.adamiec@ably.com>
2022-08-25 15:32:05 +0100

    Add queue resource

--- internal/provider/models.go
+++ internal/provider/models.go
@@ -36,3 +36,28 @@ type AblyKey struct {
 	Created    types.Int64         `tfsdk:"created"`
 	Modified   types.Int64         `tfsdk:"modified"`
 }
+
+// Ably Queue
+type AblyQueue struct {
+	AppID     types.String `tfsdk:"app_id"`
+	ID        types.String `tfsdk:"id"`
+	Name      types.String `tfsdk:"name"`
+	Ttl       types.Int64  `tfsdk:"ttl"`
+	MaxLength types.Int64  `tfsdk:"max_length"`
+	Region    types.String `tfsdk:"region"`
+
+	AmqpUri                  types.String `tfsdk:"amqp_uri"`
+	AmqpQueueName            types.String `tfsdk:"amqp_queue_name"`
+	StompURI                 types.String `tfsdk:"stomp_uri"`
+	StompHost                types.String `tfsdk:"stomp_host"`
+	StompDestination         types.String `tfsdk:"stomp_destination"`
+	State                    types.String `tfsdk:"state"`
+	MessagesReady            types.Int64  `tfsdk:"messages_ready"`
+	MessagesUnacknowledged   types.Int64  `tfsdk:"messages_unacknowledged"`
+	MessagesTotal            types.Int64  `tfsdk:"messages_total"`
+	StatsPublishRate         types.Int64  `tfsdk:"stats_publish_rate"`
+	StatsDeliveryRate        types.Int64  `tfsdk:"stats_delivery_rate"`
+	StatsAcknowledgementRate types.Int64  `tfsdk:"stats_acknowledgement_rate"`
+	Deadletter               types.Bool   `tfsdk:"deadletter"`
+	DeadletterID             types.String `tfsdk:"deadletter_id"`
+}
