b2b485cb40436a078ec0238fdf12d780e37bc4b8
Graham Russell <graham.russell@ably.com>
2022-09-16 11:07:16 +0100

    Terraform Formatting - Terraform fmt for templates (kinesis/lambda/SQS)

--- examples/playground/key.tf
+++ examples/playground/key.tf
@@ -2,8 +2,8 @@ resource "ably_api_key" "api_key_0" {
   app_id = ably_app.app0.id
   name   = "key-0000"
   capabilities = {
-    "channel2" = ["publish"],
-    "channel3" = ["subscribe"],
+    "channel2"  = ["publish"],
+    "channel3"  = ["subscribe"],
     "channel33" = ["subscribe"],
   }
 }
