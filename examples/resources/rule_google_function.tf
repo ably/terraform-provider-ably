7f080f74d3d2f70d1e4b2b26d4b9e5dadae7ef03
Morgan Adamiec <morgan.adamiec@ably.com>
2022-09-21 09:56:07 +0100

    Add google_function example resources

---
+++ examples/resources/rule_google_function.tf
@@ -0,0 +1,24 @@
+resource "ably_rule_google_function" "google_function" {
+  app_id       = ably_app.app0.id
+  status       = "enabled"
+  source = {
+    channel_filter = "^my-channel.*",
+    type           = "channel.message"
+  }
+  target       = {
+    headers = [
+      {
+        name  = "User-Agent"
+        value = "user-agent-string"
+      },
+      {
+        name  = "headerName"
+        value = "headerValue"
+      }
+    ]
+    signing_key_id = ably_api_key.api_key_0.id
+    project_id = "foo"
+    region = "us"
+    function_name = "bar"
+  }
+}
