cbd705071d1f233415b9604390f2fd4a5a0e166c
Ivan Kavalerov <ivan@kavalerov.net>
2022-09-29 09:39:23 +0100

    Add docs

--- examples/resources/app.tf
+++ examples/resources/app.tf
@@ -3,15 +3,3 @@ resource "ably_app" "app0" {
   status   = "enabled"
   tls_only = true
 }
-
-resource "ably_app" "app1" {
-  name                      = "ably-tf-provider-app-0001"
-  status                    = "enabled"
-  tls_only                  = true
-  fcm_key                   = "AABBQ1KyxCE:APA91bCCYs7r_Q-sqW8HMP_hV4t3vMYx...cJ8344-MhGWODZEuAmg_J4MUJcVQEyDn...I"
-  apns_certificate          = "-----BEGIN CERTIFICATE-----MIIFaDCC...EXAMPLE...3Dc=-----END CERTIFICATE-----"
-  apns_privateKey           = "-----BEGIN PRIVATE KEY-----ABCFaDCC...EXAMPLE...3Dc=-----END PRIVATE KEY-----"
-  apns_use_sandbox_endpoint = false
-}
-
-
