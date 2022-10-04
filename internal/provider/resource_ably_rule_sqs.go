5ec2729d1e7e9273591d33313f586c99afb0990e
Morgan Adamiec <morgan.adamiec@ably.com>
2022-10-04 12:04:40 +0100

    Refactor rule impl to a generic function
    Also fixes #46 by including the kind of resource that caused the error

--- internal/provider/resource_ably_rule_sqs.go
+++ internal/provider/resource_ably_rule_sqs.go
@@ -2,11 +2,8 @@ package ably_control
 
 import (
 	"context"
-	"fmt"
-	"strings"
 
 	"github.com/hashicorp/terraform-plugin-framework/diag"
-	"github.com/hashicorp/terraform-plugin-framework/path"
 	tfsdk_provider "github.com/hashicorp/terraform-plugin-framework/provider"
 	tfsdk_resource "github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
@@ -53,165 +50,35 @@ type resourceRuleSqs struct {
 	p provider
 }
 
+func (r *resourceRuleSqs) Provider() *provider {
+	return &r.p
+}
+
+func (r *resourceRuleSqs) Name() string {
+	return "AWS Sqs"
+}
+
 // Create a new resource
 func (r resourceRuleSqs) Create(ctx context.Context, req tfsdk_resource.CreateRequest, resp *tfsdk_resource.CreateResponse) {
-	// Checks whether the provider and API Client are configured. If they are not, the provider responds with an error.
-	if !r.p.configured {
-		resp.Diagnostics.AddError(
-			"Provider not configured",
-			"The provider hasn't been configured before apply",
-		)
-		return
-	}
-
-	// Gets plan values
-	var p AblyRuleDecoder[*AblyRuleTargetSqs]
-	diags := req.Plan.Get(ctx, &p)
-	resp.Diagnostics.Append(diags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	plan := p.Rule()
-	plan_values := GetPlanRule(plan)
-
-	// Creates a new Ably Rule by invoking the CreateRule function from the Client Library
-	rule, err := r.p.client.CreateRule(plan.AppID.Value, &plan_values)
-	if err != nil {
-		resp.Diagnostics.AddError(
-			"Error creating Resource",
-			"Could not create resource, unexpected error: "+err.Error(),
-		)
-		return
-	}
-
-	response_values := GetRuleResponse(&rule, &plan)
-
-	// Sets state for the new Ably App.
-	diags = resp.State.Set(ctx, response_values)
-	resp.Diagnostics.Append(diags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
+	CreateRule[AblyRuleTargetSqs](&r, ctx, req, resp)
 }
 
 // Read resource
 func (r resourceRuleSqs) Read(ctx context.Context, req tfsdk_resource.ReadRequest, resp *tfsdk_resource.ReadResponse) {
-	// Gets the current state. If it is unable to, the provider responds with an error.
-	var s AblyRuleDecoder[*AblyRuleTargetSqs]
-	diags := req.State.Get(ctx, &s)
-	resp.Diagnostics.Append(diags...)
-
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	state := s.Rule()
-
-	// Gets the Ably App ID and Ably Rule ID value for the resource
-	app_id := state.AppID.Value
-	rule_id := state.ID.Value
-
-	// Get Rule data
-	rule, _ := r.p.client.Rule(app_id, rule_id)
-
-	response_values := GetRuleResponse(&rule, &state)
-
-	// Sets state to app values.
-	diags = resp.State.Set(ctx, &response_values)
-
-	resp.Diagnostics.Append(diags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
+	ReadRule[AblyRuleTargetSqs](&r, ctx, req, resp)
 }
 
-// Update resource
+// // Update resource
 func (r resourceRuleSqs) Update(ctx context.Context, req tfsdk_resource.UpdateRequest, resp *tfsdk_resource.UpdateResponse) {
-	// Gets plan values
-	var p AblyRuleDecoder[*AblyRuleTargetSqs]
-	diags := req.Plan.Get(ctx, &p)
-	resp.Diagnostics.Append(diags...)
-
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	var s AblyRuleDecoder[*AblyRuleTargetSqs]
-	diags = req.State.Get(ctx, &s)
-	resp.Diagnostics.Append(diags...)
-
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	plan := p.Rule()
-	state := s.Rule()
-
-	rule_values := GetPlanRule(plan)
-
-	// Gets the Ably App ID and Ably Rule ID value for the resource
-	app_id := state.AppID.Value
-	rule_id := state.ID.Value
-
-	// Update Ably Rule
-	rule, _ := r.p.client.UpdateRule(app_id, rule_id, &rule_values)
-
-	response_values := GetRuleResponse(&rule, &plan)
-
-	// Sets state to app values.
-	diags = resp.State.Set(ctx, &response_values)
-
-	resp.Diagnostics.Append(diags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
+	UpdateRule[AblyRuleTargetSqs](&r, ctx, req, resp)
 }
 
 // Delete resource
 func (r resourceRuleSqs) Delete(ctx context.Context, req tfsdk_resource.DeleteRequest, resp *tfsdk_resource.DeleteResponse) {
-	// Gets the current state. If it is unable to, the provider responds with an error.
-	var s AblyRuleDecoder[*AblyRuleTargetSqs]
-	diags := req.State.Get(ctx, &s)
-	resp.Diagnostics.Append(diags...)
-
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	state := s.Rule()
-
-	// Gets the Ably App ID and Ably Rule ID value for the resource
-	app_id := state.AppID.Value
-	rule_id := state.ID.Value
-
-	err := r.p.client.DeleteRule(app_id, rule_id)
-	if err != nil {
-		resp.Diagnostics.AddError(
-			"Error deleting Resource",
-			"Could not delete resource, unexpected error: "+err.Error(),
-		)
-		return
-	}
-
-	// Remove resource from state
-	resp.State.RemoveResource(ctx)
+	DeleteRule[AblyRuleTargetSqs](&r, ctx, req, resp)
 }
 
 // Import resource
 func (r resourceRuleSqs) ImportState(ctx context.Context, req tfsdk_resource.ImportStateRequest, resp *tfsdk_resource.ImportStateResponse) {
-	// Save the import identifier in the id attribute
-	// identifier should be in the format app_id,key_id
-	idParts := strings.Split(req.ID, ",")
-
-	if len(idParts) != 2 || idParts[0] == "" || idParts[1] == "" {
-		resp.Diagnostics.AddError(
-			"Unexpected Import Identifier",
-			fmt.Sprintf("Expected import identifier with format: 'app_id,rule_id'. Got: %q", req.ID),
-		)
-		return
-	}
-	// Recent PR in TF Plugin Framework for paths but Hashicorp examples not updated - https://github.com/hashicorp/terraform-plugin-framework/pull/390
-	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("app_id"), idParts[0])...)
-	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("id"), idParts[1])...)
+	ImportRule(&r, ctx, req, resp)
 }
--- internal/provider/rules.go
+++ internal/provider/rules.go
@@ -1,7 +1,12 @@
 package ably_control
 
 import (
+	"context"
+	"fmt"
+	"strings"
+
 	ably_control_go "github.com/ably/ably-control-go"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	tfsdk_resource "github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
@@ -584,4 +589,175 @@ func GetKafkaAuthSchema(headers []AblyRuleHeaders) []ably_control_go.Header {
 	}
 
 	return ret_headers
+}
+
+type Rule interface {
+	Provider() *provider
+	Name() string
+}
+
+// Create a new resource
+func CreateRule[T any](r Rule, ctx context.Context, req tfsdk_resource.CreateRequest, resp *tfsdk_resource.CreateResponse) {
+	// Checks whether the provider and API Client are configured. If they are not, the provider responds with an error.
+	if !r.Provider().configured {
+		resp.Diagnostics.AddError(
+			"Provider not configured",
+			"The provider hasn't been configured before apply",
+		)
+		return
+	}
+
+	// Gets plan values
+	var p AblyRuleDecoder[*T]
+	diags := req.Plan.Get(ctx, &p)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	plan := p.Rule()
+	plan_values := GetPlanRule(plan)
+
+	// Creates a new Ably Rule by invoking the CreateRule function from the Client Library
+	rule, err := r.Provider().client.CreateRule(plan.AppID.Value, &plan_values)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			fmt.Sprintf("Error creating Resource '%s'", r.Name()),
+			fmt.Sprintf("Could not create resource '%s', unexpected error: %s", r.Name(), err.Error()),
+		)
+
+		return
+	}
+
+	response_values := GetRuleResponse(&rule, &plan)
+
+	// Sets state for the new Ably App.
+	diags = resp.State.Set(ctx, response_values)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+// Read resource
+func ReadRule[T any](r Rule, ctx context.Context, req tfsdk_resource.ReadRequest, resp *tfsdk_resource.ReadResponse) {
+	// Gets the current state. If it is unable to, the provider responds with an error.
+	var s AblyRuleDecoder[*T]
+	diags := req.State.Get(ctx, &s)
+	resp.Diagnostics.Append(diags...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	state := s.Rule()
+
+	// Gets the Ably App ID and Ably Rule ID value for the resource
+	app_id := s.AppID.Value
+	rule_id := s.ID.Value
+
+	// Get Rule data
+	rule, _ := r.Provider().client.Rule(app_id, rule_id)
+
+	response_values := GetRuleResponse(&rule, &state)
+
+	// Sets state to app values.
+	diags = resp.State.Set(ctx, &response_values)
+
+	resp.Diagnostics.Append(diags...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+// // Update resource
+func UpdateRule[T any](r Rule, ctx context.Context, req tfsdk_resource.UpdateRequest, resp *tfsdk_resource.UpdateResponse) {
+	// Gets plan values
+	var p AblyRuleDecoder[*AblyRuleTargetAmqpExternal]
+	diags := req.Plan.Get(ctx, &p)
+	resp.Diagnostics.Append(diags...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var s AblyRuleDecoder[*AblyRuleTargetAmqpExternal]
+	diags = req.State.Get(ctx, &s)
+	resp.Diagnostics.Append(diags...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	state := s.Rule()
+	plan := p.Rule()
+
+	rule_values := GetPlanRule(plan)
+
+	// Gets the Ably App ID and Ably Rule ID value for the resource
+	app_id := state.AppID.Value
+	rule_id := state.ID.Value
+
+	// Update Ably Rule
+	rule, _ := r.Provider().client.UpdateRule(app_id, rule_id, &rule_values)
+
+	response_values := GetRuleResponse(&rule, &plan)
+
+	// Sets state to app values.
+	diags = resp.State.Set(ctx, &response_values)
+
+	resp.Diagnostics.Append(diags...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+// Delete resource
+func DeleteRule[T any](r Rule, ctx context.Context, req tfsdk_resource.DeleteRequest, resp *tfsdk_resource.DeleteResponse) {
+	// Gets the current state. If it is unable to, the provider responds with an error.
+	var s AblyRuleDecoder[T]
+	diags := req.State.Get(ctx, &s)
+	resp.Diagnostics.Append(diags...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	state := s.Rule()
+
+	// Gets the Ably App ID and Ably Rule ID value for the resource
+	app_id := state.AppID.Value
+	rule_id := state.ID.Value
+
+	err := r.Provider().client.DeleteRule(app_id, rule_id)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			fmt.Sprintf("Error deleting Resource %s'", r.Name()),
+			fmt.Sprintf("Could not delete resource '%s', unexpected error: %s", r.Name(), err.Error()),
+		)
+		return
+	}
+
+	// Remove resource from state
+	resp.State.RemoveResource(ctx)
+}
+
+// // Import resource
+func ImportRule(r Rule, ctx context.Context, req tfsdk_resource.ImportStateRequest, resp *tfsdk_resource.ImportStateResponse) {
+	// Save the import identifier in the id attribute
+	// identifier should be in the format app_id,key_id
+	idParts := strings.Split(req.ID, ",")
+
+	if len(idParts) != 2 || idParts[0] == "" || idParts[1] == "" {
+		resp.Diagnostics.AddError(
+			"Unexpected Import Identifier",
+			fmt.Sprintf("Expected import identifier with format: 'app_id,rule_id'. Got: %q", req.ID),
+		)
+		return
+	}
+	// Recent PR in TF Plugin Framework for paths but Hashicorp examples not updated - https://github.com/hashicorp/terraform-plugin-framework/pull/390
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("app_id"), idParts[0])...)
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("id"), idParts[1])...)
 }
