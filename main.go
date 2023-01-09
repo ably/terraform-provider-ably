1bd7ec24daf43230fc075c7c5ea2a5f9eff1f03d
Lewis Marshall <lewis.marshall@ably.com>
2023-01-09 19:00:00 +0000

    Add a VERSION constant
    Signed-off-by: Lewis Marshall <lewis.marshall@ably.com>

--- main.go
+++ main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
 
 	ably_control "github.com/ably/terraform-provider-ably/internal/provider"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
 
+// VERSION is the version of this provider.
+//
+// It defaults to "dev" but is overridden during the release process using
+// ldflags (e.g. go build -ldflags="-X main.VERSION=x.y.z").
+var VERSION = "dev"
+
 func main() {
+	// print the version and exit if argv[1] is "version"
+	if len(os.Args) > 1 && os.Args[1] == "version" {
+		fmt.Println("terraform-provider-ably " + VERSION)
+		os.Exit(0)
+	}
 
 	opts := providerserver.ServeOpts{
 		Address: "registry.terraform.io/ably/terraform-provider-ably",
